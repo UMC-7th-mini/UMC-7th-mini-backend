1776255bff2e9ec063543974662b5be6cd29323a
Gjuni <kmj06178@gmail.com>
2024-11-19 17:26:38 +0900

    user info userKey changed

--- src/user/info/user.info.controller.js
+++ src/user/info/user.info.controller.js
@@ -1,24 +1,21 @@
-import { validateUserInput, validateUserOutput } from "../dtos/user.dto.js";
+import HttpException from "../../middlewares/errorHandler.js";
 import { getUserInfoService } from "./user.info.service.js";
 
 
 
 export const getUserInfo = async (req, res, next) => { 
     try {
-    
-        validateUserInput(req.body.id);
+        const userKey = req.body.userKey;
 
-        const user = await getUserInfoService(req.body.id);
+        const user = await getUserInfoService(userKey);
 
         if(!user){
-            return next(HttpException(404, "User not found"));
+            return next(new HttpException(404, "User not found"));
         }
-        validateUserOutput(user);
 
         return res.status(200).json({ success: true, data: user });
 
     } catch(error) {
-
         next(error);
     }
 };
